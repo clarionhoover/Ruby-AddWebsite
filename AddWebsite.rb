f4ce7c35e7a668668325ec27aa0d55a42a8abd5c
rhoover720 <randy.hoover@gmail.com>
2013-10-16 21:00:42 -0700

    making progress

--- AddWebsite.rb
+++ AddWebsite.rb
@@ -5,6 +5,9 @@
 #  Last Modified: 2013-10-14                      #
 ###################################################
 
+# HighLine for making prompting easier
+require 'highline/import'
+
 # check to see if we're running as root
 raise 'Must run as root' unless Process.uid ==0
 
@@ -12,12 +15,27 @@ raise 'Must run as root' unless Process.uid ==0
 def user_exists(username)
 	username.strip
 	username.downcase
-	File.directory? "/home/{username}"
+	File.directory? "/home/#{username}"
 end
 
-def site_exists(hostname,tld)
+def site_exists(username,hostname,tld)
 	hostname.strip
 	hostname.downcase
 	tld.strip
 	tld.downcase
+	# check to see if user already has a folder and error if folder exists
+	if (File.directory? "/home/#{username}/webroot/#{hostname}.#{tld}" == true}
+	    puts "Folder exists for user #{username} and website #{hostname}.#{tld} please delete or pick a new hostname"
+	    exit
+	end
+end
+
+username = ask "Username of the account the website will go under: "
+hostname = ask "Hostname without tld (ie clarionhoover test.clarionhoover): "
+tld = ask "TLD for hostname (ie com co org without the .): "
+
+# kill the script if user doesn't exist
+if (user_exists(username) == false)
+    puts "User doesn't exist, please create user"
+    exit
 end
